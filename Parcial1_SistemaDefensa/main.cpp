75acc947e031add9d5cc8c5fd7a8b267aa95f379
Carlos Alfredo Pinto <coordcientifico@clinicasagradocorazon.local>
2020-10-17 17:43:33 -0500

    Intenté pero no me dió... Hasta aquí llegué

--- Parcial1_SistemaDefensa/main.cpp
+++ Parcial1_SistemaDefensa/main.cpp
@@ -24,7 +24,7 @@ int main(){
         switch (opcion) {
             case '1':
                 float velocidades[]={};
-                float angulos[]={};
+                int angulos[]={};
                 cout<<"Generar disparos ofensivos que comprometan la integridad del "
                           "canon defensivo"<<endl;
                 cout<<"Ingresa la posicion en X del canon Ofensivo"<<endl;
@@ -38,8 +38,8 @@ int main(){
                 int indiceposiciones=0;
 
                 while(indiceposiciones <=3){
-                    for (float i=1;i<100 ; i=i+2.5 ) { //rango de velocidades
-                        for (float j=1;j<181 ; j=j+5) { //rango de angulos
+                    for (float i=1;i<100 ; i=i+0.1) { //rango de velocidades
+                        for (int j=1;j<181 ; j++) { //rango de angulos
                             float xDO=calcular_x(i,j,posicion_x_O);
                             float yDO=calcular_y(i,j,posicion_y_O);
 
@@ -53,7 +53,6 @@ int main(){
                                 velocidades[indiceposiciones]=i;
                                 angulos[indiceposiciones]=j;
                                 indiceposiciones++;
-                                break;
                             }
                         }
                     }
